60fb88592dd662306f68ab5c347edb4f29c6adc6
Moamen Sherif <102827938+MoamenSherif81@users.noreply.github.com>
2022-08-08 20:01:26 +0200

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,33 @@
+var swiper = new Swiper(".movies-container", {
+  slidesPerView: 4,
+  spaceBetween: 15,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
+  breakpoints:{
+    0: {
+      slidesPerView: 1 ,
+    },
+    520: {
+      slidesPerView: 2
+    },
+    950: {
+      slidesPerView: 3,
+    },
+    1500:{
+      slidesPerView: 4,
+    },
+    1600:{
+      slidesPerView: 5,
+    }
+  }
+});
