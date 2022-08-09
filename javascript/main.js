7b453028d636c9ef7c38162fe9971e736b99d00a
Moamen Sherif <102827938+MoamenSherif81@users.noreply.github.com>
2022-08-09 14:04:48 +0200

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -15,10 +15,10 @@ var swiper = new Swiper(".movies-container", {
 
   breakpoints:{
     0: {
-      slidesPerView: 1 ,
+      slidesPerView: 1,
     },
     520: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     950: {
       slidesPerView: 3,
@@ -31,3 +31,21 @@ var swiper = new Swiper(".movies-container", {
     }
   }
 });
+
+const change = element => {
+  var elementPrev = element.parentNode.parentNode.parentNode.children[0];
+  var imgChild = element.children[0];
+  var blackChild = element.children[1];
+  if(!(element.children[1].classList.contains('black-active'))){
+    Array.from(document.querySelectorAll('.black')).forEach((el) => (el.style.opacity = 0, el.classList.remove('black-active')));
+    blackChild.classList.add('black-active');
+    elementPrev.src = imgChild.src;
+    elementPrev.style.transition = "0s";
+    elementPrev.style.opacity = 0;
+    setTimeout(function(){
+      elementPrev.style.transition = "1s";
+      elementPrev.style.opacity = 1;
+    }, 10);
+    blackChild.style.opacity = 0.7;
+  }
+}
