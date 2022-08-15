e2e6dc2f3239827b0bed2af2c4dabfd47c10a155
Moamen Sherif <102827938+MoamenSherif81@users.noreply.github.com>
2022-08-15 17:00:14 +0200

    Add files via upload

---
+++ mob/js/main-mob.js
@@ -0,0 +1,32 @@
+var up_swiper = new Swiper(".card-systems", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  initialSlide: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var filmsSwiper = new Swiper('.card-effect', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  initialSlide: 2,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 100,
+    depth: 70,
+    modifier: 1,
+  }
+
+});
